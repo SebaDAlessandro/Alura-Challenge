9102af056823331864d6b0ce235f3c9836d5b979
Sebastián D´Alessandro <sebadalessandro@hotmail.com>
2022-04-27 15:33:23 -0300

    programa funcional

--- javaScript/desencriptar.js
+++ javaScript/desencriptar.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let btnDesencriptar = document.querySelector('#btn-desencriptar');
 
 btnDesencriptar.addEventListener('click',(e)=>{
@@ -62,4 +63,70 @@ btnDesencriptar.addEventListener('click',(e)=>{
 
     let form = document.querySelector('#formulario')
     form.reset()
+=======
+let btnDesencriptar = document.querySelector('#btn-desencriptar');
+
+btnDesencriptar.addEventListener('click',(e)=>{
+
+    e.preventDefault();
+
+    document.getElementById("mostrar-imagenes-resultado").style.display = "none";
+    let pantallaResultado = document.getElementById("mostrar-texto")
+    pantallaResultado.classList.remove('borrar')
+    pantallaResultado.classList.add('resultadosTexto')
+    let elementP = document.createElement('p')
+
+    let texto = document.querySelector('#mensaje')
+    let codigo = (texto.value);
+    let mensaje = []
+
+    for (let i = 0; i < codigo.length; i++) {
+             
+        switch (codigo[i]) {
+            case 'a':
+                mensaje = mensaje + 'a';
+                i=i+1;
+                break;
+            case 'e':
+                mensaje = mensaje+ 'e';
+                i=i+4;
+                break;
+            case 'i':
+                mensaje = mensaje + 'i';
+                i=i+3;
+                break;
+            case 'o':
+                mensaje = mensaje + 'o';
+                i=i+3;
+                break;
+            case 'u':
+                mensaje = mensaje + 'u';
+                i=i+3;
+                break;
+
+            default:
+                mensaje = mensaje + codigo[i];
+                break;
+        }
+    }
+    elementP.textContent = mensaje;
+    pantallaResultado.appendChild(elementP)
+    
+    function copy(m) {
+        let inputElement = document.createElement('input');
+        inputElement.setAttribute('value',m)
+        document.body.appendChild(inputElement);
+        inputElement.select();
+        document.execCommand('copy')
+        inputElement.parentNode.removeChild(inputElement);
+    }
+    
+    document.querySelector('#btn-copiar').onclick = function(){
+        copy(mensaje);
+        elementP.textContent = '';
+    }
+
+    let form = document.querySelector('#formulario')
+    form.reset()
+>>>>>>> 1a5a20e (programa funcional)
 })
